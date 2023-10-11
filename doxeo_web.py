11fe9f8b7748198633b063d0b79317204f2f6663
Moubotred <147648379+Moubotred@users.noreply.github.com>
2023-10-11 21:31:03 +0000

    Add files via upload
    -se puedo acceder a la siguiete pagina linea 77 hasta 80

--- doxeo_web.py
+++ doxeo_web.py
@@ -1,116 +1,109 @@
-import reflex as rx
-from doxeo_web import prefex,secion
-
-
-# class State(rx.State): 
-#     email:bool = False
-#     op : str = ""
-#     def resp(self):
-#         if self.op == "kivi":
-#             self.email = not (self.email)
-            
-
-# def noman() -> rx.Component:
-#     return rx.box(
-#         style = prefex.index
-#     )
-    
-
-# def index():
-#     return rx.box(
-#         rx.input(placeholder="estado de colores",
-#                 value= State.op,
-#                 on_change = State.set_op,
-#                 ),
-#         rx.button('send',type_="submit",on_click=State.resp),
-#             rx.cond(
-#                 State.email,
-#                 rx.box(noman()),
-#                 rx.text("red",color = "red")        
-#         ),
-#     )
-
-# app = rx.App()
-# app.add_page(index)
-# app.compile()
-
-
-
-
-class State(rx.State): 
-    usuario: str = ""
-    password : str = ""
-    estado : bool = False
-    def home(self):
-        if self.usuario == "kivi" and self.password == "bin123":
-            self.estado = not (self.estado)
-
-
-def about()-> rx.Component:
-    return rx.box("adios")
-
-
-def principal()-> rx.Component:
-     return rx.hstack("hola",style = secion.index_p)
-
-
-def login() -> rx.Component:
-    return rx.box(
-        rx.image(src="ixon.png",style = prefex.icono),
-        rx.input(placeholder="usuario",style = prefex.usuario,value = State.usuario,on_change = State.set_usuario),
-        rx.input(placeholder="contrasena",style = prefex.contrasena,value = State.password,on_change = State.set_password),  
-        rx.button('iniciar secion',type_="submit",on_click = State.home,style = prefex.boton_login),
-            rx.cond(
-                State.estado,
-                rx.box(principal()),
-                rx.box(about())        
-        ),
-        # rx.button("iniciar sesion",on_click = State.home,style = prefex.boton_login),
-        # rx.cond(State.estado,
-        #         rx.box(principal()),
-        #         rx.text("verde",color = "green")
-        #         ),
-        style = prefex.login
-        )
-    
-def index():
-    return rx.hstack(
-        login(),
-        style = prefex.index)
-
-app = rx.App()
-app.add_page(index,title="Noman")
-app.add_page(principal,title="Noman")
-app.compile()
-
-
-
-
-
 # import reflex as rx
 # from doxeo_web import prefex
 
 # class State(rx.State): 
-#     pass
+#     usuario: str = ""
+#     password : str = ""
+#     estado: bool = False
+#     def home(self):
+#         if self.usuario == "kivi" and self.password == "bin123":
+#             self.estado = True
 
-# @rx.page(route='/about')
+
+# @rx.page("/principal")
 # def principal():
-#     return rx.box("hola")
-#     pass
+#     return rx.hstack(
+#         rx.box("hola")
+#     )
+    
+# @rx.page("/about")
+# def about():
+#     return rx.hstack(
+#         rx.box("hola")
+#     )
 
+
+# @rx.page(route="/home", title="Home Page")
+# def login() -> rx.Component:
+#     return rx.box(
+#         rx.image(src="ixon.png",style = prefex.icono),
+#         rx.input(placeholder="usuario",style = prefex.usuario,value = State.usuario,on_change = State.set_usuario),
+#         rx.input(placeholder="contrasena",style = prefex.contrasena,value = State.password,on_change = State.set_password),  
+#         rx.button('iniciar secion',type_="submit",on_click = State.home,style = prefex.boton_login),
+#         rx.cond(
+#             State.estado,
+#             rx.link(rx.button("continuar",href = "/principal")),
+#             rx.text("hola",color = "red")
+#         ),
+#         style = prefex.login
+#         )
 
 # def index():
-#     return rx.box(
-#                 rx.input(placeholder = "usuario",style = prefex.usuario),
-#                 rx.input(placeholder = "contrasena",style = prefex.contrasena),
-#                 rx.link(
-#                     rx.button("Example"),
-#                     href="http://localhost:3000/about",
-#                     color="rgb(107,99,246)",
-#                     button=True,
-#                 )
-#     )
+#     return rx.hstack(
+#         login(),
+#         style = prefex.index)    
+
 
 # app = rx.App()
 # app.add_page(index)
+# # app.add_page(principal)
+# # app.add_page(about)
 # app.compile()
+
+
+
+import reflex as rx
+from doxeo_web import prefex
+
+class State(rx.State): 
+    usuario: str = ""
+    password : str = ""
+    estado: bool = False
+    def home(self):
+        if self.usuario == "kivi" and self.password == "bin123":
+            self.estado = True
+
+# Página de Inicio
+@rx.page("/")
+def index():
+    return rx.box(
+        rx.box( 
+            rx.image(src="ixon.png",style = prefex.icono),
+            rx.input(placeholder="usuario",style = prefex.usuario,value = State.usuario,on_change = State.set_usuario),
+            rx.input(placeholder="contrasena",style = prefex.contrasena,value = State.password,on_change = State.set_password),
+            rx.button('iniciar secion',type_="submit",on_click = State.home,style = prefex.boton_login),
+            style = prefex.login,
+                ),
+            rx.cond(
+                State.estado,
+                rx.link(rx.button("iniciar secion"),href="/principal"),
+                rx.text(""),
+                # rx.link(rx.button("about"),href="/about"),
+                ),
+        style = prefex.index
+        )
+
+@rx.page("/principal")
+def principal():
+    return rx.box(
+        rx.hstack("hola",
+            style = prefex.index
+        )
+    )
+
+# Página 'About'
+@rx.page("/about")
+def about():
+    return rx.hstack("Estás en la página 'About'")
+
+app = rx.App()
+app.add_page(index)
+app.add_page(about)
+app.add_page(principal)
+app.compile()
+
+# crea 2 paginas una con un saludo y otra con adios que en la pgina de saludo tenga un boton que me rediriga a la pagina del adios 
+# uso de redirect para para la redireccion de direfentes paginas ejemplos completos para poder usar nose nada de reflex explicame todo y no te olvides de poner el codigo completo 
+# como usar rutas y usr redirect para principiantes nose nada de como se usa codigo completo
+# 2 paginas y usar el decorador para llamar a una de las 2 paginas 
+# termina el codigo ya que falta la forma de aceder a la otra pagina con un boton no te olvides de uar el decorador de @rx.page
